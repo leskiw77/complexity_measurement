674351c9c45f5ba2a515e953b3fd0ca680ab84eb
jarema <jarema.leskiw@gmail.com>
2017-05-05 21:08:10 +0200

    initial commit

--- complexity_measurement/__init__.py
+++ complexity_measurement/__init__.py
@@ -3,22 +3,23 @@ import matplotlib.pyplot as plt
 from complexity_measurement.complexity_indicator import complexity_solver
 from complexity_measurement.runtime_measurement import runtime_calculation
 
-rc = runtime_calculation()
+def run():
+    rc = runtime_calculation()
 
-try:
-    x, y = rc.measure_reliance(10000, 10)
-except:
-    print("Execution last too long, only %d measurements acquired" % len(rc.y))
-    if len(rc.y) < 5:
-        print("Program must be terminate")
-        exit()
+    try:
+        x, y = rc.measure_reliance(10000, 10)
+    except:
+        print("Execution last too long, only %d measurements acquired" % len(rc.y))
+        if len(rc.y) < 5:
+            print("Program must be terminate")
+            exit()
 
-    print("Program wont be terminated, but result might be incorrect")
-    y = rc.y
-    x = rc.x[:len(y)]
+        print("Program wont be terminated, but result might be incorrect")
+        y = rc.y
+        x = rc.x[:len(y)]
 
-cs = complexity_solver(x, y)
-cs.compute_res()
+    cs = complexity_solver(x, y)
+    cs.compute_res()
 
-plt.plot(x, y, 'ro')
-plt.show()
+    plt.plot(x, y, 'ro')
+    plt.show()
